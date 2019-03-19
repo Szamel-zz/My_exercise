09fc7cfd01852d4c6159e5fe9b7acf4eeedfc0cf
Tamas Szamel <tamas_szamel@epam.com>
2019-03-19 16:56:31 +0100

    file_remover class unit test

--- MockTraining/file_remover.py
+++ MockTraining/file_remover.py
@@ -5,3 +5,13 @@ import os.path
 def rm(filename):
     if os.path.isfile(filename):
         os.remove(filename)
+
+
+class RemovalService(object):
+    """A service for removing objects from the filesystem."""
+
+    def rmc(filename):
+        if os.path.isfile(filename):
+            os.remove(filename)
+
+
--- tests/unit_tests/test_file_remover.py
+++ tests/unit_tests/test_file_remover.py
@@ -1,4 +1,4 @@
-from MockTraining.file_remover import rm
+from MockTraining.file_remover import rm, RemovalService
 
 
 import os
@@ -33,6 +33,31 @@ class RmTestCase(unittest.TestCase):
         mock_path.isfile.return_value = True
 
         rm("any path")
+
+        mock_os.remove.assert_called_with("any path")
+
+
+
+class RemovalServiceTestCase(unittest.TestCase):
+
+    @mock.patch('MockTraining.file_remover.os.path')
+    @mock.patch('MockTraining.file_remover.os')
+    def test_rmc(self, mock_os, mock_path):
+        # instantiate our service
+        reference = RemovalService()
+
+        # set up the mock
+        mock_path.isfile.return_value = False
+
+        reference.rmc("any path")
+
+        # test that the remove call was NOT called.
+        self.assertFalse(mock_os.remove.called, "Failed to not remove the file if not present.")
+
+        # make the file 'exist'
+        mock_path.isfile.return_value = True
+
+        reference.rmc("any path")
 
         mock_os.remove.assert_called_with("any path")
 
