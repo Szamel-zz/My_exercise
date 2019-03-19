4afe721d41ecf744ec19fd02f22e6b8dd87052f5
Tamas Szamel <tamas_szamel@epam.com>
2019-03-19 16:12:08 +0100

    test mock_patch

--- tests/unit_tests/test_mock_patch.py
+++ tests/unit_tests/test_mock_patch.py
@@ -1,17 +1,5 @@
-from MockTraining.file_remover import rm
-
-
 import os
 import mock
-import unittest
-print(type(rm))
-
-class RmTestCase(unittest.TestCase):
-    @mock.patch('MockTraining.file_remover.os')
-    def test_rm(self, mock_os):
-        rm("any path")
-        # test that rm called os.remove with the right parameters
-        mock_os.remove.assert_called_with("any path")
 
 
 @mock.patch("os.listdir", mock.MagicMock(return_value="test1"))
