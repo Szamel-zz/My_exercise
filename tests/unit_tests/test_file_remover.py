b25f5e7dcec5f0ec6bd094b013f823a8523b891e
Tamas Szamel <tamas_szamel@epam.com>
2019-03-19 16:40:17 +0100

    improved file_remover unit test

--- tests/unit_tests/test_file_remover.py
+++ tests/unit_tests/test_file_remover.py
@@ -6,9 +6,33 @@ import mock
 import unittest
 print(type(rm))
 
+
 class RmTestCase(unittest.TestCase):
+
     @mock.patch('MockTraining.file_remover.os')
     def test_rm(self, mock_os):
         rm("any path")
         # test that rm called os.remove with the right parameters
         mock_os.remove.assert_called_with("any path")
+
+
+class RmTestCase(unittest.TestCase):
+
+    @mock.patch('MockTraining.file_remover.os.path')
+    @mock.patch('MockTraining.file_remover.os')
+    def test_rm(self, mock_os, mock_path):
+        # set up the mock
+        mock_path.isfile.return_value = False
+
+        rm("any path")
+
+        # test that the remove call was NOT called.
+        self.assertFalse(mock_os.remove.called, "Failed to not remove the file if not present.")
+
+        # make the file 'exist'
+        mock_path.isfile.return_value = True
+
+        rm("any path")
+
+        mock_os.remove.assert_called_with("any path")
+
